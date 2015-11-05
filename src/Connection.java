78d3533130e716ac0e43df8c5b979c04fa95ff9b
ann_shcher <annnuta_2011@ukr.net>
2015-11-06 00:20:01 +0200

    add some new methods:reject(),accept(),sendNickHello/Busy

--- src/Connection.java
+++ src/Connection.java
@@ -1,10 +1,74 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Connection {
-    int a = 64;
-    String b=null;
+    private ServerSocket ss;
+    private Socket socket;
+    private Thread serverThread;//оборобка сервер-сокету
+    private final static int PORT = 28411;
+    private final static String CODING = "28411";
+    DataInputStream in;
+    DataOutputStream out;
 
-    //Початок
-    public static void main(String [] args){
-        System.out.println("Priva");
+    public Connection(Socket s) throws IOException {
+        this.socket = s;
+       // in = new DataInputStream(s.getInputStream());
+      //  out = new DataOutputStream(s.getOutputStream());
     }
+
+    Scanner scanner = new Scanner(System.in);
+
+
+    public ServerSocket getSs() {
+        return ss;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSs(ServerSocket ss) {
+        this.ss = ss;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+
+
+    public void sendNickHello( String nick) throws IOException { //
+        socket.getOutputStream().write(("ChatApp 2015 user "+ nick + "\n").getBytes(CODING));
+    }
+
+    public void sendNickBusy(String nick) throws IOException{
+        socket.getOutputStream().write(("ChatApp 2015 user " + nick + "busy\n").getBytes(CODING));
+    }
+
+    public void accept() throws IOException {//прийняти з"єднання
+       // socket = ss.accept();
+        socket.getOutputStream().write(("Accepted\n").getBytes(CODING));
+    }
+
+    public void reject() throws IOException{
+        socket.getOutputStream().write(("Rejected\n").getBytes(CODING));
+    }
+
+    public void sendMessage(String mes){
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
